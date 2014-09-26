a59a6688d840ac2a0b7b53567510c73ba2cf6973
Raman Aggarwal <kuberjsr@gmail.com>
2014-09-26 16:51:12 +0530

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,16 @@
+
+     function Draw()
+    {
+      var chars = "0123456789 ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var string_length = 4;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) 
+	{
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	
+      
+    } 
+	
+	
