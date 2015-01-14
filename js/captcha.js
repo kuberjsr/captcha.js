6c9c318b5a4a1d09afc8e63d63f6ef9136440082
Raman Aggarwal <kuberjsr@gmail.com>
2015-01-14 12:09:28 +0530

    Update captcha.js

--- js/captcha.js
+++ js/captcha.js
@@ -1,16 +1,15 @@
 
      function Draw()
     {
-      var chars = "0123456789 ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+      var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
 	var string_length = 4;
 	var randomstring = '';
-	for (var i=0; i<string_length; i++) 
-	{
+	for (var i=0; i<string_length; i++) {
 		var rnum = Math.floor(Math.random() * chars.length);
 		randomstring += chars.substring(rnum,rnum+1);
 	}
 	
-      
-    } 
-	
+    document.getElementById('cap2').value=randomstring;    
+    }   
+	  
 	
